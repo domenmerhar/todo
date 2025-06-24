364c52a4d076f2bb4b95b442d035fc5126b8c97d
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-24 14:54:25 +0200

    Add layout.tsx

---
+++ app/(auth-forms)/layout.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/components/ui/card";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <section>
+      <div className="flex min-h-screen items-center justify-center p-4 bg--">
+        <Card className="w-full max-w-md">{children}</Card>
+      </div>
+    </section>
+  );
+}
