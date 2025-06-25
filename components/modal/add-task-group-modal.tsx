d802c27b3e18df8cd1a2b1b7150909216d6f5eb7
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-25 12:24:48 +0200

    Add IconCombobox and ColorPicker components to AddTaskGroupModal

--- components/modal/add-task-group-modal.tsx
+++ components/modal/add-task-group-modal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   DialogClose,
@@ -10,6 +12,8 @@ import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Switch } from "../ui/switch";
+import IconCombobox from "../combobox/icon-combobox";
+import { ColorPicker } from "../color-picker";
 
 export default function AddTaskGroupModal() {
   return (
@@ -24,10 +28,18 @@ export default function AddTaskGroupModal() {
           <Input id="name" name="name" placeholder="Shopping list" />
         </div>
 
+        <IconCombobox />
+
+        <div className="flex items-center justify-between ml-0.5">
+          <Label htmlFor="color">Icon Color</Label>
+          <ColorPicker name="color" size={"icon"} />
+        </div>
+
         <div className="flex justify-between ml-0.5 space-y-4">
           <Label htmlFor="public">Public Access</Label>
-          <Switch name="public" id="public" defaultValue={1} />
+          <Switch name="public" id="public" className="scale-115 mr-1" />
         </div>
+
         <DialogFooter>
           <DialogClose asChild>
             <Button variant="outline">Cancel</Button>
