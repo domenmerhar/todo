314bc2e6d55f9b8e60068a3b75753d33d85b519d
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-25 11:30:49 +0200

    Add IconCombobox component for rendering icons with customizable display

---
+++ components/combobox/icon-combobox.tsx
@@ -0,0 +1,21 @@
+import { ICONS } from "@/lib/constants/icons";
+import { comboboxRenderFn } from "@/lib/types/combobox";
+import React from "react";
+import { Combobox } from "../ui/combobox";
+
+const renderIconOption: comboboxRenderFn = (option) => {
+  const Icon = option.value;
+
+  const formattedName = option.name.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Icon className="h-4 w-4 mr-2 inline-block" />
+      <span>{formattedName}</span>
+    </div>
+  );
+};
+
+export default function IconCombobox() {
+  return <Combobox options={ICONS} renderFn={renderIconOption} />;
+}
