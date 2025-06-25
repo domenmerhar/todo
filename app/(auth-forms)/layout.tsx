759e7253f20564ad1ed2e04404670488d91c1c73
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-25 10:04:48 +0200

    Remove background class from layout container

--- app/(auth-forms)/layout.tsx
+++ app/(auth-forms)/layout.tsx
@@ -3,7 +3,7 @@ import { Card } from "@/components/ui/card";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      <div className="flex min-h-screen items-center justify-center p-4 bg--">
+      <div className="flex min-h-screen items-center justify-center p-4">
         <Card className="w-full max-w-md">{children}</Card>
       </div>
     </section>
