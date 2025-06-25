1362a2f3d7223d9f684fcfc89b573c683678f13b
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-25 12:20:19 +0200

    Refactor icon name formatting to use normalisePascalCase utility function

--- components/combobox/icon-combobox.tsx
+++ components/combobox/icon-combobox.tsx
@@ -2,11 +2,12 @@ import { ICONS } from "@/lib/constants/icons";
 import { comboboxRenderFn } from "@/lib/types/combobox";
 import React from "react";
 import { Combobox } from "../ui/combobox";
+import { normalisePascalCase } from "@/lib/util/string-utils";
 
 const renderIconOption: comboboxRenderFn = (option) => {
   const Icon = option.value;
 
-  const formattedName = option.name.replace(/([a-z])([A-Z])/g, "$1 $2");
+  const formattedName = normalisePascalCase(option.name);
 
   return (
     <div className="flex items-center space-x-2">
