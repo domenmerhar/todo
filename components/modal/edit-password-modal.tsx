f36df9c776f723b0e6b1bcf9c308019ed6f13bb9
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-29 20:55:23 +0200

    Add Edit Password Modal component for user password management

--- components/modal/edit-password-modal.tsx
+++ components/modal/edit-password-modal.tsx
@@ -1,41 +1,11 @@
 import React from "react";
-import {
-  DialogClose,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "../ui/dialog";
-import { Button } from "../ui/button";
-import PasswordInput from "../password-input";
+import { DialogContent } from "../ui/dialog";
+import EditPasswordForm from "../form/edit-password-form";
 
 export default function EditPasswordModal() {
   return (
     <DialogContent className="sm:max-w-[425px]">
-      <form className="space-y-4">
-        <DialogHeader>
-          <DialogTitle>Edit Password</DialogTitle>
-        </DialogHeader>
-
-        <PasswordInput placeholder="Enter your password" required id="password">
-          Password
-        </PasswordInput>
-
-        <PasswordInput
-          placeholder="Confirm your password"
-          required
-          id="confirm-password"
-        >
-          Confirm Password
-        </PasswordInput>
-
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DialogClose>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </form>
+      <EditPasswordForm />
     </DialogContent>
   );
 }
