005441bbdf61a1f1450fa444a919539520d698c8
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-29 20:55:55 +0200

    Refactor Sign In page to utilize SignInForm component

--- app/(auth-forms)/sign-in/page.tsx
+++ app/(auth-forms)/sign-in/page.tsx
@@ -7,13 +7,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Label } from "@radix-ui/react-label";
-import { Input } from "@/components/ui/input";
 import Link from "next/link";
-import PasswordInput from "@/components/password-input";
 import BackButton from "@/components/back-button";
 import { Metadata } from "next";
-import { signIn } from "@/actions/auth";
+import SignInForm from "@/components/form/sign-in-form";
 
 export const metadata: Metadata = {
   title: "Sign in",
@@ -33,32 +30,10 @@ export default function Component() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form className="space-y-4" action={signIn}>
-          <div className="space-y-2">
-            <Label htmlFor="username">Username</Label>
-            <Input
-              id="username"
-              name="username"
-              type="text"
-              placeholder="Enter your username"
-              required
-            />
-          </div>
+        <SignInForm />
 
-          <PasswordInput
-            placeholder="Enter your password"
-            required
-            id="password"
-          >
-            Password
-          </PasswordInput>
-
-          <Button type="submit" className="w-full">
-            Sign in
-          </Button>
-        </form>
         <div className="mt-4 text-center text-sm text-muted-foreground">
-          Don&apos;t have an account?{" "}
+          Don&apos;t have an account?
           <Link href="/sign-up" className="text-primary hover:underline">
             Sign up
           </Link>
