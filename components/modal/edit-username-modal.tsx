ebf53ed7bf92ad47cbe9659128b2001689665664
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-29 20:55:27 +0200

    Add Edit Username Modal component for user username management

--- components/modal/edit-username-modal.tsx
+++ components/modal/edit-username-modal.tsx
@@ -1,35 +1,11 @@
 import React from "react";
-import {
-  DialogClose,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "../ui/dialog";
-import { Label } from "@radix-ui/react-label";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
+import { DialogContent } from "../ui/dialog";
+import EditUsernameForm from "../form/edit-username-form";
 
 export const EditUsernameModal = () => {
   return (
     <DialogContent className="sm:max-w-[425px]">
-      <form className="space-y-2">
-        <DialogHeader>
-          <DialogTitle>Edit Username</DialogTitle>
-        </DialogHeader>
-
-        <Label htmlFor="name" className="block">
-          Username
-        </Label>
-        <Input id="username" name="username" placeholder="johnny007" />
-
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DialogClose>
-          <Button type="submit">Save Changes</Button>
-        </DialogFooter>
-      </form>
+      <EditUsernameForm />
     </DialogContent>
   );
 };
