61f11582c2213b8b2fca187c93b82b4c87e5c6dc
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-29 20:55:49 +0200

    Refactor Sign Up page to use SignUpForm component

--- app/(auth-forms)/sign-up/page.tsx
+++ app/(auth-forms)/sign-up/page.tsx
@@ -1,25 +1,22 @@
 import type React from "react";
 
-import { Button } from "@/components/ui/button";
 import {
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Label } from "@radix-ui/react-label";
-import { Input } from "@/components/ui/input";
 import Link from "next/link";
-import PasswordInput from "@/components/password-input";
 import BackButton from "@/components/back-button";
 import { Metadata } from "next";
+import SignUpForm from "@/components/form/sign-up-form";
 
 export const metadata: Metadata = {
   title: "Sign up",
   description: "Create a new account to access the application.",
 };
 
-export default function Component() {
+export default function Page() {
   return (
     <>
       <CardHeader>
@@ -32,38 +29,7 @@ export default function Component() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="username">Username</Label>
-            <Input
-              id="username"
-              name="username"
-              type="text"
-              placeholder="Enter your username"
-              required
-            />
-          </div>
-
-          <PasswordInput
-            placeholder="Enter your password"
-            required
-            id="password"
-          >
-            Password
-          </PasswordInput>
-
-          <PasswordInput
-            placeholder="Confirm your password"
-            required
-            id="confirm-password"
-          >
-            Confirm Password
-          </PasswordInput>
-
-          <Button type="submit" className="w-full">
-            Create Account
-          </Button>
-        </form>
+        <SignUpForm />
 
         <div className="mt-4 text-center text-sm text-muted-foreground">
           Already have an account?{" "}
