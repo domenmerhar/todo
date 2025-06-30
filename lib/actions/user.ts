c94dc42dafdaf3967f5a23e97e3f25880b48380a
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-30 12:59:30 +0200

    Refactor editUsername and changePassword functions to improve error handling and session management

--- lib/actions/user.ts
+++ lib/actions/user.ts
@@ -1,5 +1,11 @@
 "use server";
 
+import { headers } from "next/headers";
+import { auth } from "../auth";
+import pool from "../db/pool";
+import { getSession } from "./auth";
+import { redirect } from "next/navigation";
+
 export async function editUsername(
   _prevState: { values: { username: string }; errors: string[] },
   formData: FormData
@@ -9,9 +15,28 @@ export async function editUsername(
   let error: string = "";
   if (!username.trim()) error = "Username cannot be empty";
 
-  if (error) return { error, values: { username } };
+  if (error) return { success: false, error, values: { username } };
+
+  const session = await getSession();
+  if (!session || !session.user || !session.user.id) redirect("/sing-in");
+
+  try {
+    await pool.query(`UPDATE "user" SET name = $1 WHERE id = $2`, [
+      username,
+      session.user.id,
+    ]);
+  } catch (error) {
+    console.error("Error updating username:", error);
+
+    return {
+      success: false,
+      error: "Failed to update username. Please try again.",
+      values: { username },
+    };
+  }
 
   return {
+    success: true,
     error: "",
     values: { username },
   };
@@ -19,15 +44,13 @@ export async function editUsername(
 
 export async function changePassword(
   _prevState: {
-    values: { password: string; confirmPassword: string };
+    values: { password: string; newPassword: string };
     errors: string[];
   },
   formData: FormData
 ) {
   const password = (formData.get("password") as string) || "";
-  const confirmPassword = (formData.get("confirm-password") as string) || "";
-
-  console.log(confirmPassword);
+  const newPassword = (formData.get("new-password") as string) || "";
 
   const errors: string[] = [];
 
@@ -35,18 +58,30 @@ export async function changePassword(
   else if (password.length < 8)
     errors.push("Password must be at least 8 characters long");
 
-  if (!confirmPassword.trim()) errors.push("Confirm Password cannot be empty");
-  else if (confirmPassword.length < 8)
+  if (!newPassword.trim()) errors.push("Confirm Password cannot be empty");
+  else if (newPassword.length < 8)
     errors.push("Confirm Password must be at least 8 characters long");
 
-  if (errors.length === 0 && password !== confirmPassword)
-    errors.push("Passwords do not match");
-
   if (errors.length > 0)
-    return { errors, values: { password, confirmPassword } };
+    return { success: false, errors, values: { password, newPassword } };
+
+  try {
+    await auth.api.changePassword({
+      body: { newPassword, currentPassword: password },
+      headers: await headers(),
+    });
+  } catch (error) {
+    console.error("Error changing password:", error);
+    return {
+      success: false,
+      errors: ["Failed to change password. Please try again."],
+      values: { password, newPassword },
+    };
+  }
 
   return {
+    success: true,
     errors: [],
-    values: { password, confirmPassword },
+    values: { password, newPassword },
   };
 }
