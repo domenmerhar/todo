113452575cd47c2d357f40ae0081354fc5dcce17
Domen Merhar <domen.merhar@students.vegova.si>
2025-06-30 12:04:32 +0200

    Add authentication configuration with better-auth and username plugin; remove old auth index file

---
+++ lib/auth.ts
@@ -0,0 +1,26 @@
+import { betterAuth } from "better-auth";
+import { username } from "better-auth/plugins/username";
+import { nextCookies } from "better-auth/next-js";
+import pool from "./db/pool";
+
+export const auth = betterAuth({
+  plugins: [
+    username({ minUsernameLength: 3, maxUsernameLength: 20 }),
+    nextCookies(),
+  ],
+
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60,
+    },
+
+    storeSessionInDatabase: true,
+  },
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: 6,
+  },
+
+  database: pool,
+});
