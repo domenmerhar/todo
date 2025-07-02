17c784d5812d3153a16036b7376e74989c319932
Domen Merhar <domen.merhar@students.vegova.si>
2025-07-02 19:33:31 +0200

    Refactor AddTaskGroupModal to integrate form handling and state management with validation

--- components/modal/add-task-group-modal.tsx
+++ components/modal/add-task-group-modal.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import React from "react";
+import React, { useActionState, useEffect } from "react";
 import {
-  DialogClose,
   DialogContent,
   DialogFooter,
   DialogHeader,
@@ -14,18 +13,44 @@ import { Input } from "../ui/input";
 import { Switch } from "../ui/switch";
 import IconCombobox from "../combobox/icon-combobox";
 import { ColorPicker } from "../color-picker";
+import { addTodoGroup } from "@/lib/actions/task-group";
+import Form from "next/form";
+import ErrorList from "../error-list";
+import { Spinner } from "../ui/spinner";
+import { toast } from "sonner";
 
 export default function AddTaskGroupModal() {
+  const [state, formAction, isPending] = useActionState(addTodoGroup, {
+    success: false,
+    errors: [],
+    values: {
+      name: "",
+      icon: "ListTodo",
+      color: "blue",
+      public: "off",
+    },
+  });
+
+  useEffect(() => {
+    toast.success("Task group added successfully!");
+  }, [state.success]);
+
   return (
     <DialogContent className="sm:max-w-[425px]">
-      <form className="space-y-4">
+      <Form formMethod="POST" action={formAction} className="space-y-4">
         <DialogHeader>
           <DialogTitle>Add Task Group</DialogTitle>
         </DialogHeader>
 
         <div className="space-y-2">
           <Label htmlFor="name">Task Group Name</Label>
-          <Input id="name" name="name" placeholder="Shopping list" required />
+          <Input
+            id="name"
+            name="name"
+            placeholder="Shopping list"
+            defaultValue={state.values.name}
+            required
+          />
         </div>
 
         <IconCombobox />
@@ -37,16 +62,26 @@ export default function AddTaskGroupModal() {
 
         <div className="flex justify-between ml-0.5 space-y-4">
           <Label htmlFor="public">Public Access</Label>
-          <Switch name="public" id="public" className="scale-115 mr-1" />
+          <Switch
+            name="public"
+            id="public"
+            className="scale-115 mr-1"
+            defaultValue={state.values.public === "on" ? 1 : 0}
+          />
         </div>
 
+        <ErrorList errors={state.errors} />
+
         <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DialogClose>
-          <Button type="submit">Create</Button>
+          <Button variant="outline" disabled={isPending}>
+            {isPending ? <Spinner className="text-gray-100" /> : "Cancel"}
+          </Button>
+
+          <Button type="submit" disabled={isPending}>
+            {isPending ? <Spinner className="text-gray-100" /> : "Save Changes"}
+          </Button>
         </DialogFooter>
-      </form>
+      </Form>
     </DialogContent>
   );
 }
