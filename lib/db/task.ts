fe29312f01b7b4aebffc9c9ef61a1de27b925246
Domen Merhar <domen.merhar@students.vegova.si>
2025-07-04 12:45:21 +0200

    Add getUserTaskCount function for user-specific task count retrieval

--- lib/db/task.ts
+++ lib/db/task.ts
@@ -1,11 +1,53 @@
 import "server-only";
 import { query } from ".";
+import { getSession } from "../actions/auth";
+import { DEV_PROMISE_DELAY } from "../constants";
 
 export interface Task {
   id: number;
   group_id: number;
   task_name: string;
   finsihed: boolean;
+}
+
+interface GetUserTaskCountResponse {
+  success: boolean;
+  count?: number;
+  error?: string;
+}
+
+export async function getUserTaskCount(): Promise<GetUserTaskCountResponse> {
+  await new Promise((resolve) => setTimeout(resolve, DEV_PROMISE_DELAY));
+
+  const session = await getSession();
+  if (!session?.user?.id)
+    return { success: false, error: "User not authenticated" };
+
+  let res;
+
+  try {
+    res = await query(
+      `
+        SELECT COUNT(*)
+        FROM "Task"
+        WHERE "group_id"
+        IN (SELECT "id"
+        FROM "Group"
+        WHERE "user_id" = $1);
+      `,
+      [session.user.id]
+    );
+  } catch (error) {
+    console.error("Error fetching task count:", error);
+    return { success: false, error: "Failed to fetch task count" };
+  }
+
+  if (!res || !res.rowCount) return { success: false, error: "No tasks found" };
+
+  return {
+    success: true,
+    count: res.rows[0],
+  };
 }
 
 interface GetGroupTasksResponse {
@@ -17,7 +59,7 @@ interface GetGroupTasksResponse {
 export async function getGroupTasks(
   groupId: string
 ): Promise<GetGroupTasksResponse> {
-  await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulate delay
+  await new Promise((resolve) => setTimeout(resolve, DEV_PROMISE_DELAY)); // Simulate delay
   let res;
 
   try {
