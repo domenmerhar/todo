304ea6e9aabf71715ceccaf964e3b71b3c65baae
Domen Merhar <domen.merhar@students.vegova.si>
2025-07-04 12:31:43 +0200

    Add GroupTitle component to display group information with icon and styling

---
+++ components/drag-and-drop/group-title.tsx
@@ -0,0 +1,24 @@
+import { ICONS } from "@/lib/constants/icons";
+import { getGroupById } from "@/lib/db/group";
+import React, { CSSProperties } from "react";
+
+export default async function GroupTitle({ groupId }: { groupId: string }) {
+  const res = await getGroupById(groupId);
+
+  if (!res.success || !res.group)
+    return <div className="text-red-500">{res.error || "Group not found"}</div>;
+
+  const { color, group_name: groupName, icon } = res.group;
+
+  const Icon = ICONS.find(({ name }) => name === icon)?.value;
+  const iconStyle: CSSProperties = { backgroundColor: color };
+
+  return (
+    <div className="flex gap-4 items-center">
+      {Icon ? (
+        <Icon style={iconStyle} className="text-white size-12 p-2 rounded-lg" />
+      ) : null}
+      <h2 className="text-2xl font-bold">{groupName}</h2>
+    </div>
+  );
+}
