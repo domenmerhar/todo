66158cabe2b1df630ed4e426185ebb3ae2f12a6e
Domen Merhar <domen.merhar@students.vegova.si>
2025-07-05 18:21:24 +0200

    Add session check and redirect in AuthFormsLayout component

--- app/(auth-forms)/layout.tsx
+++ app/(auth-forms)/layout.tsx
@@ -1,10 +1,14 @@
 import { Card } from "@/components/ui/card";
+import { getSession } from "@/lib/actions/auth";
+import { redirect } from "next/navigation";
 
-export default function AuthFormsLayout({
+export default async function AuthFormsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  if (await getSession()) redirect("/home");
+
   return (
     <section>
       <div className="flex min-h-screen items-center justify-center p-4">
