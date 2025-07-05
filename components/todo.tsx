703cf127feda88190465ef5dcffe212aa8268d33
Domen Merhar <domen.merhar@students.vegova.si>
2025-07-05 18:21:50 +0200

    Refactor Todo component to support drag-and-drop functionality with sortable integration

--- components/todo.tsx
+++ components/todo.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Checkbox } from "./ui/checkbox";
 import { Label } from "./ui/label";
 import { IdLabelObj } from "@/lib/types/objects";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { Grip } from "lucide-react";
 
 export default function Todo({ id, label }: IdLabelObj) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style: CSSProperties = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
   return (
-    <div key={id} className="flex items-center gap-3">
-      <Checkbox id={id} />
-      <Label htmlFor={id}>{label}</Label>
+    <div
+      className="bg-gray-50 p-4 rounded shadow-md flex justify-between items-center"
+      ref={setNodeRef}
+      style={style}
+    >
+      <div className="flex items-center gap-4">
+        <Checkbox id={String(id)} />
+        <Label htmlFor={String(id)}>{label}</Label>
+      </div>
+      <Grip {...attributes} {...listeners} className="cursor-move" />
     </div>
   );
 }
