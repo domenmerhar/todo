4ccbc21487fb81d39b961b35adea62e851ac4d3c
Domen Merhar <domen.merhar@students.vegova.si>
2025-07-06 19:39:08 +0200

    Fix incorrect environment variable for database port configuration

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -5,7 +5,7 @@ export const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
-  port: parseInt(process.env.DB_PORT || "5432", 10),
+  port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
 });
 
 export const query = async (text: string, params: unknown[]) => {
